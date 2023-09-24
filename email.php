10077e449768faa0a4b86c607323dedbdb4a0869
SenchaBka <aburiak@my.centennialcollege.ca>
2023-09-24 10:36:02 -0400

    Add all the code without validation

--- email.php
+++ email.php
@@ -1,9 +1,24 @@
 <?php
 
-$subject = $_POST["subject"];
-$number = $_POST["number"];
-$place = $_POST["place"];
-$date = $_POST["date"];
-$additional_info = $_POST["additional_info"];
+    $name = $_POST["name"];
+    $subject = $_POST["subject"];
+    $number = $_POST["number"];
+    $place = $_POST["place"];
+    $date = $_POST["date"];
+    $additional_info = $_POST["additional_info"];
+
+
+    $to = "senchab@ukr.net";
+    $emailSubject = "Photoshoot Appointment";
+    $body = "";
+
+    $body .= "Name: " .$name. "\r\n";
+    $body .= "Subject: " .$subject. "\r\n";
+    $body .= "Number of people: " .$number. "\r\n";
+    $body .= "Place: " .$place. "\r\n";
+    $body .= "Date and time: " .$date. "\r\n";
+    $body .= "Additional details: " .$additional_info. "\r\n";
+
+    mail($to, $emailSubject, $body);
 
 ?>
