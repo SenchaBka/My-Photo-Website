25c4f49c5327111e07a0c8aa1a380f8ffe7b9e9b
SenchaBka <aburiak@my.centennialcollege.ca>
2023-09-24 10:49:57 -0400

    Add sanitizing of variables

--- email.php
+++ email.php
@@ -1,24 +1,27 @@
 <?php
 
-    $name = $_POST["name"];
-    $subject = $_POST["subject"];
-    $number = $_POST["number"];
-    $place = $_POST["place"];
-    $date = $_POST["date"];
-    $additional_info = $_POST["additional_info"];
+    if(isset($_POST['name']) && !empty($_POST['name'])){
 
-
-    $to = "senchab@ukr.net";
-    $emailSubject = "Photoshoot Appointment";
-    $body = "";
-
-    $body .= "Name: " .$name. "\r\n";
-    $body .= "Subject: " .$subject. "\r\n";
-    $body .= "Number of people: " .$number. "\r\n";
-    $body .= "Place: " .$place. "\r\n";
-    $body .= "Date and time: " .$date. "\r\n";
-    $body .= "Additional details: " .$additional_info. "\r\n";
-
-    mail($to, $emailSubject, $body);
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
+        $subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_SPECIAL_CHARS);
+        $number = filter_input(INPUT_POST, "number", FILTER_SANITIZE_SPECIAL_CHARS);
+        $place = filter_input(INPUT_POST, "place", FILTER_SANITIZE_SPECIAL_CHARS);
+        $date = filter_input(INPUT_POST, "date", FILTER_SANITIZE_SPECIAL_CHARS);
+        $additional_info = filter_input(INPUT_POST, "additional_info", FILTER_SANITIZE_SPECIAL_CHARS);
+    
+    
+        $to = "senchab@ukr.net";
+        $emailSubject = "Photoshoot Appointment";
+        $body = "";
+    
+        $body .= "Name: " .$name. "\r\n";
+        $body .= "Subject: " .$subject. "\r\n";
+        $body .= "Number of people: " .$number. "\r\n";
+        $body .= "Place: " .$place. "\r\n";
+        $body .= "Date and time: " .$date. "\r\n";
+        $body .= "Additional details: " .$additional_info. "\r\n";
+    
+        mail($to, $emailSubject, $body);
+    }
 
 ?>
