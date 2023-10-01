d675fbecf9cab77694adc8a2de223c79a04fe173
SenchaBka <aburiak@my.centennialcollege.ca>
2023-10-01 19:03:25 -0400

    Changes code based on the PHPMailer library

--- email.php
+++ email.php
@@ -1,9 +1,14 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+    require 'phpmailer/src/SMTP.php';
 
     $message_sent = false;
 
     if(isset($_POST['name']) && !empty($_POST['name'])){
-
         $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
         $contacts = filter_input(INPUT_POST, "contacts", FILTER_SANITIZE_SPECIAL_CHARS);
         $subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_SPECIAL_CHARS);
@@ -12,25 +17,41 @@
         $date = filter_input(INPUT_POST, "date", FILTER_SANITIZE_SPECIAL_CHARS);
         $additional_info = filter_input(INPUT_POST, "additional_info", FILTER_SANITIZE_SPECIAL_CHARS);
     
-    
-        $to = "senchab@ukr.net";
         $emailSubject = "Photoshoot Appointment";
         $body = "";
     
-        $body .= "Name: " .$name. "\r\n";
-        $body .= "Subject: " .$subject. "\r\n";
-        $body .= "Number of people: " .$number. "\r\n";
-        $body .= "Place: " .$place. "\r\n";
-        $body .= "Date and time: " .$date. "\r\n";
-        $body .= "Additional details: " .$additional_info. "\r\n";
-    
-        mail($to, $emailSubject, $body);
+        $body .= "Name: " .$name. "<br>";
+        $body .= "Subject: " .$subject. "<br>";
+        $body .= "Number of people: " .$number. "<br>";
+        $body .= "Place: " .$place. "<br>";
+        $body .= "Date and time: " .$date. "<br>";
+        $body .= "Additional details: " .$additional_info. "<br>";
+
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'arsenii.buriak@gmail.com';
+        $mail->Password = 'APP-PASSWORD-HERE';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+        $mail->setFrom('arsenii.buriak@gmail.com');
+        $mail->addAddress('arsenii.buriak@gmail.com');
+        $mail->isHTML(true);
+
+        $mail->Subject = ($emailSubject);
+        $mail->Body = ($body);
+        $mail->send();
+
         $message_sent = true;
     }
 
     if ($message_sent)
         header("Location: thanks.html");
-    else
-        header("Location: appoinment.html");
-
+    else{
+        echo
+            "<script>
+                alert('Error');
+            </script>";
+    }
 ?>
